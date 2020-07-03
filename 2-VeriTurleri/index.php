bb7b592073f9d80c16bfa0903335f8378dfc35da
Enver Çelik <devcelikenver@gmail.com>
2020-07-03 21:55:51 +0300

    Veri türleri eklendi

---
+++ 2-VeriTurleri/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+-php de veri türü ,  atanan değere göre  otomatik olarak belirlenir.Özel olarak atanmaz.
+
+Veri Turleri :
+
+Ana veri türleri        -> boolean , integer , double/float , string
+Bileşik veri türleri    -> array , object
+Özel veri türleri       -> resource , null ,callable
+
+*/
+
+
+
+$isim = "enver";  //isim bir stringtir.
+$sayi = 15;       //sayi bir int değerdir.
