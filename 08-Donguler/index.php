64438aff2df045a7c3b4c1e2e06527ab60aeaadd
Enver Çelik <devcelikenver@gmail.com>
2020-07-07 12:11:15 +0300

    Donguler Eklendi

---
+++ 08-Donguler/index.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+
+//while döngüsü :
+
+while ($kosul) {
+
+}
+
+
+
+//Do while döngüsü :
+
+do {
+
+}while($kosul);
+
+
+
+
+//for döngüsü :
+
+for ($deger; $kosul; $deger) {
+
+}
+
+
+
+// foreach döngüsü : dizi üzerinde otomatik gezer.
+
+
+Yapı 1 :
+foreach ($dizi as $deger) {
+    echo $deger;
+}
+//dizideki tüm değerleri sırayla basar
+
+
+
+Yapı 2 :
+foreach ($dizi as $key => $deger) {
+    echo $key . ' => ' $deger;
+}
+//dizideki tüm key-değer ikililerini sırayla basar
+
+
+
+break;    ---> döngüden çıkar.
+continue; ---> altında kalan kısmı atlayıp döngünün bir sonraki adımına geçer.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+?>
