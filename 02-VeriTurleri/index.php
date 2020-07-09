6ca7a43d4c0f09f63f233752797bb9ead428712b
Enver Çelik <devcelikenver@gmail.com>
2020-07-09 16:58:59 +0300

    Veri Türleri Güncellendi (tip dönüşümleri eklendi)

--- 02-VeriTurleri/index.php
+++ 02-VeriTurleri/index.php
@@ -1,7 +1,8 @@
 <?php
 
-/*
 
+/*VERİ TÜRLERİ */
+/*
 -php de veri türü ,  atanan değere göre  otomatik olarak belirlenir.Özel olarak atanmaz.
 
 Veri Turleri :
@@ -16,3 +17,92 @@ Bileşik veri türleri    -> array , object
 
 $isim = "enver";  //isim bir stringtir.
 $sayi = 15;       //sayi bir int değerdir.
+
+
+
+
+
+/*VERİ TÜRÜ DÖNÜŞÜMLERİ */
+
+//gettype();  : veri tipini döner.
+//settype();  : veri tipini değiştirir.
+
+$sayi = "8";
+echo gettype($sayi);
+settype($sayi , "integer");
+echo gettype($sayi);
+
+
+
+//typecasting
+
+$sayi = "5";
+$sayi = (integer)($sayi);
+echo gettype($sayi);
+
+
+$sayi = 5.15;
+$sayi = (integer)($sayi);
+echo gettype($sayi);
+
+
+$sayi = 7.22;
+$sayi = (string)($sayi);
+echo gettype($sayi);
+
+
+$sayi = (boolean)($sayi);
+echo gettype($sayi);
+
+
+
+
+
+
+/*DEĞER VERİ TİPİ DÖNÜŞTÜRME*/
+
+//boolval();   herhangi bir değişkenin içeriğini boolean veri tipinde döndürür.(veri tipini değiştirmez)
+//intval();
+//floatval();
+//doubleval();
+
+
+$sayi = "8.55";
+echo gettype($sayi);
+
+$sayiBool = boolval($sayi);
+echo gettype($sayi);
+echo gettype($sayiBool);
+
+
+/*BAZI FONKSİYONLAR*/
+
+//var_dump();    :  bir değişkenin veri türünü , içeriği ile birliklte basar.
+//serialize();   :  bir değişkenin değerini saklanabilir veri türüne dönüştürüp döndürür.
+//unserialize(); : serialize() işlemini tersine döndürür.
+
+//not :  saklanabilir veri türü verilerin bir dosyada ya da veritabanında tutulması için daha uygundur.
+//       (diziler için kullanışlı)
+
+
+
+//Not : declare(strict_types = 1);  ifadesini sayfanın başına eklersek yazacağımız fonksiyonlara veri türü
+//        dayatması yapabiliriz. (only php 7)
+
+/*
+//sayi int olmak zorundadır aksi durumda hata verir.
+
+function (int $sayi ) {
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
